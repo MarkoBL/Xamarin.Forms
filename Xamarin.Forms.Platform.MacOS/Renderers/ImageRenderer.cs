2ee6b04578675943de37df0056a940447f6073cd
Marko B. Ludolph <marko.b.ludolph@gmail.com>
2017-07-07 17:07:15 +0200

    MacOS Image Aspect Fix

--- Xamarin.Forms.Platform.MacOS/Renderers/ImageRenderer.cs
+++ Xamarin.Forms.Platform.MacOS/Renderers/ImageRenderer.cs
@@ -6,7 +6,7 @@ using CoreGraphics;
 
 namespace Xamarin.Forms.Platform.MacOS
 {
-	public class ImageRenderer : ViewRenderer<Image, NSView>
+	public class ImageRenderer : ViewRenderer<Image, FormsNSImageView>
 	{
 		bool _isDisposed;
 
